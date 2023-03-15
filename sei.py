3235637d4391459e90bc8870cb6fef5068194a66
ErnestGeo24 <ernest1.avasi@gmail.com>
2023-03-15 13:28:59 +0000

    es 6

--- sei.py
+++ sei.py
@@ -22,5 +22,21 @@ def pastanorma():
 def pizza():
   return render_template("ricettapizza.html")
 
+@app.route('/pasta_carbonara/en')
+def pastacarbo():
+  return render_template("ricettacarbonareen.html")
+
+@app.route('/lasagne_bolognesi/en')
+def lasagne_bolognesi():
+  return render_template("ricettalasagneen.html")
+
+@app.route('/Pasta_alla_norma/en')
+def pastanorma():
+  return render_template("ricettapastanormaen.html")
+
+@app.route('/Pizza/en')
+def pizza():
+  return render_template("ricettapizzaen.html")
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
